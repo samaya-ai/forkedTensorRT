fd3d7d0214bcee191ca97301cff9be599dd59c23
Zuo <zuo@samaya.ai>
2024-08-02 12:03:26 +0100

    add files

---
+++ demo/HuggingFace/__init__.py
@@ -0,0 +1,13 @@
+# init_path.py
+import sys
+import os
+
+# Get the absolute path to the HuggingFace directory
+huggingface_root = os.path.dirname(os.path.abspath(__file__))
+
+# Add the HuggingFace directory to the sys.path
+if huggingface_root not in sys.path:
+    sys.path.append(huggingface_root)
+
+# Optional: Print sys.path for debugging
+print("Updated sys.path:", sys.path)
