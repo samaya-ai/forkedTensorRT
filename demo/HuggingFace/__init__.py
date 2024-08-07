a161fc6c0d48ac59ff8cce0e1dad7a3d8465b7fb
Zuo <zuo@samaya.ai>
2024-08-07 11:09:44 +0100

    not treat huggingface as root

--- demo/HuggingFace/__init__.py
+++ demo/HuggingFace/__init__.py
@@ -1,13 +1,13 @@
-# init_path.py
-import sys
-import os
+# # init_path.py
+# import sys
+# import os
 
-# Get the absolute path to the HuggingFace directory
-huggingface_root = os.path.dirname(os.path.abspath(__file__))
+# # Get the absolute path to the HuggingFace directory
+# huggingface_root = os.path.dirname(os.path.abspath(__file__))
 
-# Add the HuggingFace directory to the sys.path
-if huggingface_root not in sys.path:
-    sys.path.append(huggingface_root)
+# # Add the HuggingFace directory to the sys.path
+# if huggingface_root not in sys.path:
+#     sys.path.append(huggingface_root)
 
-# Optional: Print sys.path for debugging
-print("Updated sys.path:", sys.path)
+# # Optional: Print sys.path for debugging
+# print("Updated sys.path:", sys.path)
